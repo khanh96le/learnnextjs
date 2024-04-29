d4a688c48cf6eb03b224a32681cc9bfaa32547c8
khanh96le <khanh96le@gmail.com>
2024-04-30 00:26:19 +0700

    Add a little delay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -58,6 +58,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
