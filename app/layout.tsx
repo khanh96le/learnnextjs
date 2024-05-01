2e1b2737a9d4644e4fb768504d7989d948620054
khanh96le <khanh96le@gmail.com>
2024-05-01 08:31:50 +0700

    Finish chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme dashboard',
+  description: 'The official Next.JS Course Dashboard, built with App Router',
+  metadataBase: new URL('https://learnnextjs-phi.vercel.app/')
+}
 
 export default function RootLayout({
   children,
